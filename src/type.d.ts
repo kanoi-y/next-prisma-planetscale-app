b1dd066dd45f0835afc79115ef7b12aac88ffeda
kanoi-y <furafuratonbo@gmail.com>
2022-08-14 22:21:22 +0900

    🔨 schemaを作成

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,23 +1,6 @@
+import { Book, Author, User } from '@prisma/client';
+type Book = Book;
 
-type Book = {
-  id: number;
-  stripe_product_id?: string;
-  stripe_price_id?: string;
-  title: string;
-  description: string;
-  authorId: number;
-  price: number;
-};
+type Author = Author;
 
-type Author = {
-  id: number;
-  name: string;
-};
-
-type User = {
-  uid: string;
-  stripe_customer_id?: string;
-  name: string;
-  email: string;
-  plan: 'free' | 'pro';
-};
+type User = User;
